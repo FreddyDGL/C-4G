113d526cdcbd2f67acfe262007785dc4e7dc3d68
Enrique Gomez <enrique_1321124095@uptecamac.edu.mx>
2022-05-27 12:51:36 -0500

    avance2

--- Juego.js
+++ Juego.js
@@ -3,40 +3,40 @@ class Juego{
     constructor(dimensionTablero,nombre){
         this.dimensionTablero = dimensionTablero;
         this.nombre = nombre;
+        //crear una variable global que contenga el tablero
+        this.generarTablero(dimensionTablero); //Variable global del tablero
+
     }
 
-    verificarTamanio(){
-        if(this.dimensionTablero >= 5){
-            alert("El tamaño del tablero es correcto");
-            this.generarTablero
-        }
-        else {
-            alert("El tamaño del tablero es incorrecto");
-                 
-        }
-    }
 
     generarTablero(dimensionTablero){
+        //llamar el tablero
         let tablero = [];
                 for(let i = 0; i < dimensionTablero; i++){   
                     tablero.push(0);
-                }          console.log(tablero);
+                }     
+
+                console.log(tablero);
+                this.tablero = tablero;
+                // return tablero;
     }
     
 
     ingresarBarco(){
+
         //acomodar barcos
+        //llamar el tablero desde el constructor
+        let tablero = this.tablero;
         for(let i =0; i <5;i++ ){
-            let lugarBarco = prompt("Ingresa tu Barco al tablero de 1 x " + this.dimensionTablero);
+            let lugarBarco = prompt("Ingresa tu Barco al tablero de 0 x " + this.dimensionTablero);
             if(tablero[lugarBarco]==1){
                 alert("Esta casilla se encuentra ocupada");
                 i--
             }else if(tablero[lugarBarco]==0){  
             tablero[lugarBarco]=1;
-            let newtablero = tablero;
             }
         }
-        console.log(newtablero);
+        console.log(tablero);
     }  
 
 }
